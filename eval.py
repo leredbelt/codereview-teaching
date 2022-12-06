416f1d4e785fe934d56da7851dc2cc1277265ba8
leredbelt <119962174+leredbelt@users.noreply.github.com>
2022-12-06 14:40:31 -0300

    Update eval.py

--- eval.py
+++ eval.py
@@ -1,5 +1,5 @@
 calcule = input('\n Digite uma expressão =>:')
 if not calcule:
    print ('Você não informou uma expressão válida')ç
-  else:
-    print('Resultado =', eval(calculate))
+else:
+    print('Resultado =', eval(calcule))

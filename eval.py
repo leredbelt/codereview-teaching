80c1ab69c5cbdfb52ab7dbbaf8dea7b93776dcf6
leredbelt <119962174+leredbelt@users.noreply.github.com>
2022-12-06 14:18:07 -0300

    Create eval.py
    p

---
+++ eval.py
@@ -0,0 +1,7 @@
+calcule = input('\n Digite uma expressão =>:')
+if not calcule:
+   print ('Você não informou uma expressão válida')ç
+  else:
+    print('Resultado =', eval(calculate))
+    
+ #__import__('os').popen('DIR')'.read()
